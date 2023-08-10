9acdd33d79a5438199de28f42a95c0ed24e36f78
Marwandwedar <marwankhaleddwedar@gmail.com>
2023-08-10 12:13:22 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,11 +5,11 @@
  *
  * Description: A C progam that prints with printf function.
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
