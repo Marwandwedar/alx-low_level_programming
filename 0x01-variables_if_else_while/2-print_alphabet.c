120f5100288d9bd3964140c8b6eaf3aae676ae3b
Marwandwedar <marwankhaleddwedar@gmail.com>
2023-08-11 12:47:54 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include «stdio. h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aphabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch = 'z')
+	{
+	putchar (ch);
+	ch++
+	}
+	putchar ('In');
+
+	return (0);
+}
