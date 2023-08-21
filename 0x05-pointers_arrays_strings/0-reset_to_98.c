c38de6f3fe4ff675b9ede5be827ae758122e7182
Marwandwedar <marwankhaleddwedar@gmail.com>
2023-08-21 04:43:33 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * rasat_to_98 - takas a pointer to an lnt parameter
+ *		 and updates the value it points to 98
+ *@n: int parameter
+ *
+ * Return: Nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
