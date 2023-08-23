40a6681be66e7b4a96b9dfb6927f3d52640ea35c
Marwandwedar <marwankhaleddwedar@gmail.com>
2023-08-23 09:51:51 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,19 +3,19 @@
 /**
  * _strcmp - compares two strings
  * @s1: string 1
- * @s2: string 2
+ * @s2: strina 2
  * Return: 1 if true, 0 if false
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	int eqaul = 0;
+	int equal = 0;
 
 	while (*s1)
 	{
 		if (*s1 != *s2)
 		{
-			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			equal = ((int) *s1 - 48) - ((int) *s2 - 48);
 			break;
 		}
 		s1++;
