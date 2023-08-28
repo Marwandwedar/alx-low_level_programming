0116b38d269e040db2a2584844d0118226ae9915
Marwandwedar <marwankhaleddwedar@gmail.com>
2023-08-28 09:24:54 -0700

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * *_strchr - fills memory with a constant byte.
  * @s: pointer to put the constant
  * @c: constant
- * Return: pointer to 8
+ * Return: pointer to s
 */
 
 char *_strchr(char *s, char c)
@@ -20,3 +20,4 @@ char *_strchr(char *s, char c)
 	}
 
 	return ('\0');
+}
